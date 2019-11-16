ca31c98868993639e54ebd30b7382c8df704ab16
everlastlucas <everlastlucas@outlook.com>
2019-11-16 20:20:02 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        logcas.name
+      </h1>
+      <div class="links">
+        <a-tooltip placement="top">
+          <template slot="title">
+            <span>Coming Soon</span>
+          </template>
+          <a href="#" class="button--green">
+            About Me
+          </a>
+        </a-tooltip>
+        <a
+          href="https://github.com/logcas"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
